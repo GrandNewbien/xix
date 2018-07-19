10fd8ba2c7452062c375e93fe0786278725ffb0e
Tejpal Sahota <tssahota@uwaterloo.ca>
2018-07-19 16:59:56 -0400

    Fade in class toggle added

--- themes/xix/static/js/script.js
+++ themes/xix/static/js/script.js
@@ -4,33 +4,36 @@ $(document).ready(function () {
 
     // Scene Handler
     var scene1 = new ScrollMagic.Scene({
-      triggerElement: ".info1", // point of execution
+      triggerElement: ".car1", // point of execution
       duration: $(window).height(),
       triggerHook: 0, 
       reverse: true 
     })
     .setPin(".hero1") // the element we want to pin
+    .setClassToggle(".car1", "fade-in")
     .addTo(controller)
     .addIndicators();
     
     // Scene2 Handler
     var scene2 = new ScrollMagic.Scene({
-      triggerElement: ".info2", // point of execution
+      triggerElement: ".car2", // point of execution
       duration:$(window).height(), 
       triggerHook: 0,
       reverse: true
     })
     .setPin(".hero2") // the element we want to pin
+    .setClassToggle(".car2", "fade-in")
     .addTo(controller)
     .addIndicators();
 
     var scene3 = new ScrollMagic.Scene({
-        triggerElement: ".info3", // point of execution
+        triggerElement: ".car3", // point of execution
         duration:$(window).height(),
         triggerHook: 0,
         reverse: true
       })
       .setPin(".hero3") // the element we want to pin
+      .setClassToggle(".car3", "fade-in")
       .addTo(controller)
       .addIndicators();
   });
