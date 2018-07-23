b6b606a589bcfc3204ef696e26c467ec7f00d3b2
Tejpal Sahota <tssahota@uwaterloo.ca>
2018-07-23 19:24:10 -0400

    Added a slider and slight refactoring

--- themes/xix/assets/js/script.js
+++ themes/xix/assets/js/script.js
@@ -1,50 +1,60 @@
 $(document).ready(function () {
-    console.log("Test")
 
-    var responsiveDuration = "100%";
-    if ($( window ).width() <= 500) {
-      responsiveDuration = "100%"
-    };
+// ScrollMagic***************************************************
 
-    var controller = new ScrollMagic.Controller();
+  var responsiveDuration = "100%";
+  if ($( window ).width() <= 500) {
+    responsiveDuration = "100%"
+  };
 
-    // Scene Handler
-    var scene1 = new ScrollMagic.Scene({
-      triggerElement: ".car1", // point of execution
-      duration:  responsiveDuration,
-      triggerHook: 0,
-      pushFollowers: false, 
-      reverse: true 
-    })
-    .setPin(".hero1") // the element we want to pin
-    .setClassToggle(".car1", "fade-in")
-    .addTo(controller)
-    .addIndicators();
-    
-    // Scene2 Handler
-    var scene2 = new ScrollMagic.Scene({
-      triggerElement: ".car2", // point of execution
-      duration: responsiveDuration, 
+  var controller = new ScrollMagic.Controller();
+
+  // Scene Handler
+  var scene1 = new ScrollMagic.Scene({
+    triggerElement: ".car1",
+    duration:  responsiveDuration,
+    triggerHook: 0,
+    pushFollowers: false, 
+    reverse: true 
+  })
+  .setPin(".hero1")
+  .setClassToggle(".car1", "fade-in")
+  .addTo(controller)
+  .addIndicators();
+  
+  // Scene2 Handler
+  var scene2 = new ScrollMagic.Scene({
+    triggerElement: ".car2",
+    duration: responsiveDuration, 
+    triggerHook: 0,
+    pushFollowers: false, 
+    reverse: true
+  })
+  .setPin(".hero2")
+  .setClassToggle(".car2", "fade-in")
+  .addTo(controller)
+  .addIndicators();
+
+  var scene3 = new ScrollMagic.Scene({
+      triggerElement: ".car3",
+      duration: responsiveDuration,
       triggerHook: 0,
       pushFollowers: false, 
       reverse: true
     })
-    .setPin(".hero2") // the element we want to pin
-    .setClassToggle(".car2", "fade-in")
+    .setPin(".hero3")
+    .setClassToggle(".car3", "fade-in")
     .addTo(controller)
     .addIndicators();
 
-    var scene3 = new ScrollMagic.Scene({
-        triggerElement: ".car3", // point of execution
-        duration: responsiveDuration,
-        triggerHook: 0,
-        pushFollowers: false, 
-        reverse: true
-      })
-      .setPin(".hero3") // the element we want to pin
-      .setClassToggle(".car3", "fade-in")
-      .addTo(controller)
-      .addIndicators();
+
+// Slick*********************************************************
+  $('.slider').slick({
+    fade: true,
+    cssEase: 'linear'
+  });
+
+
 // **** MOBILE VIEW ****
       if ($( window ).width() <= 500) {
 
